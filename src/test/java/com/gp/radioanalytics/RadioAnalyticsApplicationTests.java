0bbd2b59816fccf5f25c46c5e805f96b2f3f1596
giuliopetteno <giulio.petteno@gmail.com>
2026-07-19 19:49:49 +0200

    Fix tests workflow issues

--- src/test/java/com/gp/radioanalytics/RadioAnalyticsApplicationTests.java
+++ src/test/java/com/gp/radioanalytics/RadioAnalyticsApplicationTests.java
@@ -1,10 +1,11 @@
 package com.gp.radioanalytics;
 
+import com.gp.radioanalytics.base.AbstractIntegrationTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class RadioAnalyticsApplicationTests {
+class RadioAnalyticsApplicationTests extends AbstractIntegrationTest {
 
 	@Test
 	void contextLoads() {
